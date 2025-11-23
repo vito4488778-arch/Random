200afce851069dd20cc55955b5615d031bb83773
vito4488778-arch <vito.4488778@gmail.com>
2025-11-23 11:52:21 +0800

    Initial Commit

---
+++ Random/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Random
+//
+//  Created by 114iosClassStudent04 on 2025/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Random/RandomApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomApp.swift
+//  Random
+//
+//  Created by 114iosClassStudent04 on 2025/11/23.
+//
+
+import SwiftUI
+
+@main
+struct RandomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
